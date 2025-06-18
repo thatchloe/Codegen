05ffa454d639088d3890c717a00172ef32ff010d
Hoang Anh <43473412+thatchloe@users.noreply.github.com>
2025-06-18 15:39:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# Fail the build if any command fails
-set -o errexit
 
-# Upgrade pip and install wheels support
-pip install --upgrade pip
+
+pip install pip==20.1.1
 pip install wheel setuptools
 
 # Install Python dependencies
